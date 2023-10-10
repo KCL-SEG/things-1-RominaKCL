cecb7bdf28243e5681a9ce47442e6d0ba0811854
Romina <k22034001@kcl.ac.uk>
2023-10-11 00:20:36 +0100

    Extend AbstractUser

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.db.models import Model
+from django.contrib.auth.models import AbstractUser
 
-class Thing():
+class Thing(AbstractUser):
     name=models.CharField(max_length=10, unique=True)
     description=models.CharField(max_length=20)
     quantity=models.IntegerField()
