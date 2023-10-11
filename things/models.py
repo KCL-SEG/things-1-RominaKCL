f643d78e6397afbd7b558a5f9449aa7df776e0cf
Romina <k22034001@kcl.ac.uk>
2023-10-11 01:17:41 +0100

    Add extra conditions to Thing model's fields

--- things/models.py
+++ things/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Thing(models.Model):
-        name=models.CharField(max_length=10, unique=True)
-        description=models.CharField(max_length=20, blank=True)
-        quantity=models.IntegerField(blank=True)
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, blank=True, unique=False)
+    quantity = models.IntegerField(
+        unique=False,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ]
+    )
+
